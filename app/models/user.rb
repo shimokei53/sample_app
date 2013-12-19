d63df9e189807dcd7ed453c84aa5281a4cb0d920
shimokei53 <respect-assistance_may.3@hotmail.co.jp>
2013-12-19 07:42:11 +0000

    add user-validation for 6.5 enshu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
 
-  before_save { self.email = email.downcase }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
